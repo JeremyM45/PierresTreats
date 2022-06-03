01586d80d84496d139a87d82357c990d59575804
JeremyM45 <jeremyvs1945@gmail.com>
2022-06-03 14:24:18 -0700

    refactor contollers imports

--- PierresTreats/Controllers/TreatsControllers.cs
+++ PierresTreats/Controllers/TreatsControllers.cs
@@ -6,8 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using System.Threading.Tasks;
-using System.Security.Claims;
 
 namespace PierresTreatsController.Controllers
 {
