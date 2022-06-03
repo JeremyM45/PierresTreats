c987ab09b0647416f40361413e525bdf263b9d5b
JeremyM45 <jeremyvs1945@gmail.com>
2022-06-03 16:47:01 -0700

    add display treats and flavors on splash page

--- PierresTreats/Controllers/HomeControllers.cs
+++ PierresTreats/Controllers/HomeControllers.cs
@@ -1,14 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PierresTreats.Controllers
 {
     public class HomeController : Controller
     {
-
+      private readonly PierresTreatsContext _db;
+      public HomeController(PierresTreatsContext db)
+      {
+        _db = db;
+      }
       [HttpGet("/")]
       public ActionResult Index()
       {
-        return View();
+        List<Treat> model = _db.Treats.ToList();
+        ViewBag.Flavors = _db.Flavors.ToList();
+        return View(model);
       }
 
     }
